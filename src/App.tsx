21f25581b1f2a1e6745757277134580b09db1559
Preston Davies <pdavies88@gmail.com>
2022-12-29 23:50:47 -0500

    Init set up

---
+++ src/App.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+
+export const sum = (a: number, b: number) => {
+  return a + b;
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>{sum(9, 2)}</p>
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+};
+
+export default App;
