4b52b87ffa2593af2f129cd09a579160972725e4
Preston Davies <pdavies@Prestons-Air-M2.fios-router.home>
2023-01-03 14:59:43 -0500

    Shopping Cart Increase Decrease Logic

---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,84 @@
+import { createContext, ReactNode, useContext, useState } from "react";
+
+type ShoppingCartProviderProps = {
+  children: ReactNode;
+};
+
+type ShoppingCartContextProps = {
+  getItemQuantity: (id: number) => number;
+  increaseCartQuantity: (id: number) => void;
+  decreaseCartQuantity: (id: number) => void;
+  removeFromCart: (id: number) => void;
+};
+
+type CartItem = {
+  id: number;
+  quantity: number;
+};
+
+const ShoppingCartContext = createContext({} as ShoppingCartContextProps);
+
+export const useShoppingCart = () => {
+  return useContext(ShoppingCartContext);
+};
+
+export const ShoppingCartProvider = ({
+  children,
+}: ShoppingCartProviderProps) => {
+  const [cartItems, setCartItems] = useState<CartItem[]>([]);
+
+  const getItemQuantity = (id: number) => {
+    return cartItems.find((item) => item.id === id)?.quantity || 0;
+  };
+
+  const increaseCartQuantity = (id: number) => {
+    setCartItems((items) => {
+      if (items.find((item) => item.id === id) == null) {
+        return [...items, { id, quantity: 1 }];
+      } else {
+        return items.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  };
+
+  const decreaseCartQuantity = (id: number) => {
+    setCartItems((items) => {
+      if (items.find((item) => item.id === id)?.quantity === 1) {
+        return items.filter((item) => item.id !== id);
+      } else {
+        return items.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 };
+          } else {
+            return item;
+          }
+        });
+      }
+    });
+  };
+
+  const removeFromCart = (id: number) => {
+    setCartItems((items) => {
+      return items.filter((item) => item.id !== id);
+    });
+  };
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart,
+      }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  );
+};
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,3 +1,20 @@
-export const Store = () => {
-  return <div>Store</div>;
+import { Col, Row } from "react-bootstrap";
+import storeItems from "../data/items.json";
+import StoreItem from "../components/StoreItem";
+
+const Store = () => {
+  return (
+    <>
+      <div>Store</div>
+      <Row xs={1} md={2} lg={3} className="g-3">
+        {storeItems.map((item) => (
+          <Col key={item.id}>
+            <StoreItem {...item} />
+          </Col>
+        ))}
+      </Row>
+    </>
+  );
 };
+
+export default Store;
