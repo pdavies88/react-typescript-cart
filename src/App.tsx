adf1802c8b7e96629b0888fddbd034ea45ee0a22
Preston Davies <pdavies@Prestons-Air-M2.fios-router.home>
2023-01-03 12:21:25 -0500

    Setup Bootstrap, Routing, Images, Navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,25 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import "./App.css";
+import { Home } from "./pages/Home";
+import { Store } from "./pages/Store";
+import { About } from "./pages/About";
+import { Navigation } from "./components/Navigation";
 
 const App = () => {
-  return <div>React Typescript Jest Starter</div>;
+  return (
+    <>
+      <Navigation />
+      <Container className="mb-4">
+        React Typescript Jest Starter
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
+  );
 };
 
 export default App;
